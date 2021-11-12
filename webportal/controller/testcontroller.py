9f18d6c1a905b77a3f6061bb34961aa86862c92c
ryangohsc <32363441+ryangohsc@users.noreply.github.com>
2021-11-12 16:46:12 +0800

    TestController to test car communication with the web portal

--- webportal/controller/testcontroller.py
+++ webportal/controller/testcontroller.py
@@ -1,6 +1,10 @@
 from flask_restful import Resource, reqparse
 
 
+parser = reqparse.RequestParser()
+parser.add_argument('name', required=True)
+
+
 class TestControllerAPI(Resource):
 	def get(self):
-		return {"test": "a"}
+		return 200

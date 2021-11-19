898b1a82da473a8cda7461c40911f3bb844abbd2
ryangohsc <32363441+ryangohsc@users.noreply.github.com>
2021-11-19 12:20:46 +0800

    Added a map controller
    The map controller provides a link for the web server to POST the map coordinates to.

---
+++ webportal/controller/mapcontroller.py
@@ -0,0 +1,11 @@
+from flask_restful import Resource, reqparse
+from webportal.models.map import *
+
+parser = reqparse.RequestParser()
+parser.add_argument('coordinates', required=True)
+
+
+class MapControllerAPI(Resource):
+	def post(self):
+		args = parser.parse_args()
+		insert_map(args['coordinates'])

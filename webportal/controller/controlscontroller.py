fe9034a7ca0285445c7f5c2400df2c1f59578e38
ryangohsc <32363441+ryangohsc@users.noreply.github.com>
2021-12-02 02:09:28 +0800

    Added fix to the carcontroller

--- webportal/controller/controlscontroller.py
+++ webportal/controller/controlscontroller.py
@@ -7,24 +7,38 @@ parser.add_argument('command', required=True)
 
 
 class ControlsControllerAPI(Resource):
+	"""
+	Class to control the behaviour of the Controls Controller API. 
+	:param: Resource. 
+	:return: None.
+	"""    	
+	def get(self):
+		"""
+		Defines the behaviour of how the Controls Controller API reacts to a HTTP GET request. 
+		:param: self. 
+		:return: 201.
+		"""    	
+		data = delete_command() 
+		return data
 
-    def get(self):
-        data = delete_command() 
-        return data
 
+	def post(self):
+		"""
+		Defines the behaviour of how the Controls Controller API reacts to a HTTP GET request. 
+		:param: self. 
+		:return: 201.
+		"""  		
+		args = parser.parse_args()
+		if args['command'] == "motor_start()":
+			command = 1
 
-    def post(self):
-    	args = parser.parse_args()
-    	if args['command'] == "motor_start()":
-    		command = 1
+		elif args['command'] == "motor_back()":
+			command = 2
 
-    	elif args['command'] == "motor_back()":
-    		command = 2
+		elif args['command'] == "motor_left()":
+			command = 3
 
-    	elif args['command'] == "motor_left()":
-    		command = 3
-
-    	elif args['command'] == "motor_right()":
-    		command = 4
-    	insert_commands(command)
-    	return 200
+		elif args['command'] == "motor_right()":
+			command = 4
+		insert_commands(command)
+		return 200
